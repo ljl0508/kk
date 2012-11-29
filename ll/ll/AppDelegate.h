574d8eff646a030067684763eb0dccb57ad381ad
ljl0508 <lijunlong0123@126.com>
2012-11-29 17:24:26 +0800

    frist

---
+++ ll/ll/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ll
+//
+//  Created by lijunlong on 12-11-29.
+//  Copyright (c) 2012年 lijunlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ll/ll/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ll
+//
+//  Created by lijunlong on 12-11-29.
+//  Copyright (c) 2012年 lijunlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
